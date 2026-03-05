0b5da080cb3feeb14906f2207a059f09445aa735
njb <nduwayezujackson300@gmail.com>
2026-03-05 16:25:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+
+// serve static files
+app.use(express.static(path.join(__dirname, "public")));
 
 let users = [];
 
+// home route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// register user
 app.post("/register", (req, res) => {
-  users.push(req.body);
-  res.json({ message: "Wiyandikishije neza" });
+  const user = req.body;
+  users.push(user);
+
+  res.json({
+    success: true,
+    message: "Wiyandikishije neza"
+  });
 });
 
+// admin login
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
@@ -24,14 +36,32 @@ app.post("/login", (req, res) => {
     return res.json({ success: true });
   }
 
-  res.json({ success: false, message: "Login failed" });
+  res.json({
+    success: false,
+    message: "Login failed"
+  });
 });
 
+// get all users
 app.get("/users", (req, res) => {
   res.json(users);
 });
 
+// delete user
+app.delete("/users/:index", (req, res) => {
+  const index = req.params.index;
+
+  if (users[index]) {
+    users.splice(index, 1);
+    res.json({ message: "User deleted" });
+  } else {
+    res.json({ message: "User not found" });
+  }
+});
+
+// port for Render
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log("Running at http://localhost:" + PORT);
+  console.log("Server running on port " + PORT);
 });
