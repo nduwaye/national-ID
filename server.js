372b5728435a87b2e721497ec938b6737ba52b0e
njb <nduwayezujackson300@gmail.com>
2026-03-05 16:15:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-let users = []; 
+let users = [];
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.post("/register", (req, res) => {
   users.push(req.body);
   res.json({ message: "Wiyandikishije neza" });
 });
+
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
@@ -19,9 +26,12 @@ app.post("/login", (req, res) => {
 
   res.json({ success: false, message: "Login failed" });
 });
+
 app.get("/users", (req, res) => {
   res.json(users);
 });
-app.listen(3000, () => {
-  console.log("Running at http://localhost:3000");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Running at http://localhost:" + PORT);
 });
